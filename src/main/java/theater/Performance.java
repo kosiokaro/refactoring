8458073876733aa5b9a00b1cece25aa649c86c13
Kosi <kosiokaro@gmail.com>
2025-11-23 19:53:36 -0500

    Task 1.1

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,29 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance of a play.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private String playId;
+    private int audience;
 
-    public Performance(String playID, int audience) {
-        this.playID = playID;
+    /**
+     * Constructs a Performance.
+     * @param playId the ID of the play being performed
+     * @param audience the size of the audience
+     */
+
+    public Performance(String playId, int audience) {
+        this.playId = playId;
         this.audience = audience;
+    }
+
+    public String getPlayId() {
+        return playId;
+    }
+
+    public int getAudience() {
+        return audience;
     }
 }
--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,28 @@
 package theater;
 
+/**
+ * Class representing a play.
+ */
 public class Play {
+    private String name;
+    private String type;
 
-    public String name;
-    public String type;
-
+    /**
+     * Constructs a Play.
+     * @param name the name of the play
+     * @param type the type/genre of the play
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
 }
